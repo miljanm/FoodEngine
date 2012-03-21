6f931adf9e6f98b040a6d578aa216b57e00cc384
kirkygamer <kirkygamer@gmail.com>
2012-03-21 00:22:11 +0000

    Adding review form

--- app/views/Restaurants.php
+++ app/views/Restaurants.php
@@ -41,6 +41,19 @@ if(empty($restaurants))
 	
 </table>
 
+<form action="thispage.php" method="post" accept-charset="utf-8">
+    <fieldset><legend>Review this Restaurant</legend>	
+    <p><label for="rating">Rating</label><input type="radio" name="rating"
+      value="5" /> 5 <input type="radio" name="rating" value="4" /> 4
+      <input type="radio" name="rating" value="3" /> 3 <input type="radio"
+      name="rating" value="2" /> 2 <input type="radio" name="rating" value="1" /> 1</p>
+    <p><label for="review">Review</label><textarea name="review" rows="8" cols="40">
+       </textarea></p>
+    <p><input type="submit" value="Submit Review"></p>
+    <input type="hidden" name="product_type" value="actual_product_type" id="product_type">
+    <input type="hidden" name="product_id" value="actual_product_id" id="product_id">
+</fieldset>
+</form>
 
 <hr style="opacity:0.3" />
 
